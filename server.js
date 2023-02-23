e92e6eaade27dd16916e2063721510d7938a8a6d
anabelaSantos <anabelamsantos@gmail.com>
2023-02-23 20:42:35 +0000

    bonus completed

--- server.js
+++ server.js
@@ -1,12 +1,34 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const app = express();
 const port = 3000;
-const {Restaurant, Menu, Item} = require("./models/index")
-const {sequelize} = require("./db")
+const { Restaurant, Menu, Item } = require("./models/index");
+const { sequelize } = require("./db");
 
-//TODO: 
+//TODO:
+
+app.get("/restaurants", async (req, res) => {
+  const restaurant = await Restaurant.findAll({
+    include: Menu, //Which model should we add here?
+    include: [
+      {
+        model: Menu,
+        include: [
+          {
+            model: Item, //Which model should we add here?
+          },
+        ],
+      },
+    ],
+  });
+
+  //     //Argument 2
+
+  //     include: [(model: Menu), (include: [item])],
+
+  res.json(restaurant);
+});
 
 app.listen(port, () => {
-    sequelize.sync()
-    console.log("App listening on port " + port)
-})
+  sequelize.sync();
+  console.log("App listening on port " + port);
+});
